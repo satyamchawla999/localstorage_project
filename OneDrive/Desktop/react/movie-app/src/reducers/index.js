8922b48d83861ddec4805f74dd9c1d4810854987
Satyan Chawla <satyamchawla999@gmail.com>
2023-05-07 00:07:55 +0530

    changing the state to object

--- OneDrive/Desktop/react/movie-app/src/reducers/index.js
+++ OneDrive/Desktop/react/movie-app/src/reducers/index.js
@@ -1,8 +1,16 @@
 import {ADD_MOVIES} from '../actions'
 
-export default function movies (state = [],action) {
+const initialState = {
+    list :[],
+    favourities:[]
+}
+
+export default function movies (state = initialState,action) {
     if(action.type === ADD_MOVIES) {
-        return action.movies;
+        return {
+            ...state,
+            list:action.movies
+        };
     }
     return state;
 }
